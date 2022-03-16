47887a4b85ec166c75a750be106f9df248ceb0ee
vood <balitsky_igor@list.ru>
2022-03-16 17:32:41 +0300

    s

---
+++ load-testing/src/test/scala/ru/vood/http/load/testing/scenario/ScenarioConst.scala
@@ -0,0 +1,15 @@
+package ru.vood.http.load.testing.scenario
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.http.request.builder.HttpRequestBuilder
+
+
+object ScenarioConst {
+
+  val coroutinesGetScenario: HttpRequestBuilder = http("coroutinesGet")
+    .get("1")
+    .check(status is 200)
+
+
+}
---
+++ load-testing/src/test/scala/ru/vood/http/load/testing/simulation/HttpSimulation.scala
@@ -0,0 +1,26 @@
+package ru.vood.http.load.testing.simulation
+
+import io.gatling.core.Predef._
+import io.gatling.core.scenario.Simulation
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.Predef.http
+import io.gatling.http.protocol.HttpProtocolBuilder
+import ru.vood.http.load.testing.scenario.ScenarioConst.coroutinesGetScenario
+
+import scala.concurrent.duration.DurationInt
+
+class HttpSimulation extends Simulation {
+
+  private val httpBuilder: HttpProtocolBuilder = http.baseUrl("http://localhost:8080/")
+
+
+  private val scn: ScenarioBuilder = scenario("asdas")
+    .exec(coroutinesGetScenario)
+
+  setUp(
+    scn
+      .inject(constantUsersPerSec(1000) during (20 seconds))
+      .protocols(httpBuilder))
+
+
+}
