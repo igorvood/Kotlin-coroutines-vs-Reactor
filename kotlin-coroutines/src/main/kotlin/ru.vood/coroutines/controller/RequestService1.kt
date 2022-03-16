4bbd67203a378a364c32d9b69565500435ded086
vood <balitsky_igor@list.ru>
2022-03-16 13:08:57 +0300

    s

--- kotlin-coroutines/src/main/kotlin/ru.vood/coroutines/controller/RequestService1.kt
+++ kotlin-coroutines/src/main/kotlin/ru.vood/coroutines/controller/RequestService1.kt
@@ -1,13 +1,17 @@
 package ru.vood.coroutines.controller
 
 import kotlinx.coroutines.delay
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class RequestService1 {
 
+    @Value("\${delay}")
+    var delayV: Long = 10
+
     suspend fun getData(id: String): String {
-        delay(1000)
+        delay(delayV)
         return "$id ${RequestService1::class.java}"
     }
 }
--- kotlin-coroutines/src/main/kotlin/ru.vood/coroutines/controller/RequestService2.kt
+++ kotlin-coroutines/src/main/kotlin/ru.vood/coroutines/controller/RequestService2.kt
@@ -1,13 +1,16 @@
 package ru.vood.coroutines.controller
 
 import kotlinx.coroutines.delay
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class RequestService2 {
+    @Value("\${delay}")
+    var delayV: Long = 10
 
     suspend fun getData(id: String): String {
-        delay(1000)
+        delay(delayV)
         return "$id ${RequestService2::class.java}"
     }
 }
